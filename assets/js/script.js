991860357e51a8fa417fe392df2edb371a08498d
webMavrick <waterpro92@yahoo.com>
2021-06-06 20:43:21 -0400

    incomplete design

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ const apiKey = "c3dbbcb04a32ec8684f7fd34432569a8";
 // storage variables
 const storageArray = [];
 const locationObj = {};
+// variables for current forecast
 let locationLatitude;
 let locationLongitude;
 let currentData;
@@ -20,18 +21,23 @@ const currentForecast = (location) => {
     const apiCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + apiKey;
     fetch(apiCurrent).then((response) => {
             response.json().then((data) => {
-                // console.log(data)
-                const date = data.dt;
-                // console.log(date)
+                // convert date value from api data
+                let currentDate = new Date();
+                console.log(currentDate)
+                let options = {
+                    month: "numeric",
+                    day: "numeric"
+                };
+                const formattedDate = currentDate.toLocaleDateString("en-US", options);
                 currentData = {
                     name: data.name,
-                    date: "",
+                    date: formattedDate,
                     temp: data.main.temp,
                     wind: data.wind.speed + " MPH",
                     humidity: data.main.humidity + "%",
                     uv: ""
                 }
-                $("#currentTitle").text(currentData.name);
+                $("#currentTitle").text(currentData.name + "(" + formattedDate + ")");
                 $("#temp").text("Temp: " + currentData.temp);
                 $("#wind").text("Wind: " + currentData.wind);
                 $("#humid").text("Humidity: " + currentData.humidity);
@@ -42,50 +48,60 @@ const currentForecast = (location) => {
                 fiveDayForecast(inputData.value, locationLatitude, locationLongitude);
             })
         })
-};
-
-// function to fetch and display five day forcast
-const fiveDayForecast = (location, latitude, longitude) => {
-    const apiFiveDay = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=" + apiKey;
-    fetch(apiFiveDay).then((response) => {
-        response.json().then((data) => {
-           console.log(data)
-           for(i = 1; i <= 5; i++) {
-           fiveDayData = {
-               name: location,
-               date: data.daily[i].dt,
-               temp: data.daily[i].temp.day,
-               wind: data.daily[i].wind_speed + " MPH",
-               humid: data.daily[i].humidity + "%",
-               uv: data.daily[i].uvi
-           }
-        //  display data
-        const forecastCard = $("#day" + [i]);
-        forecastCard.append($("<li>").text(fiveDayData.name).
-                     append($("<li>").text(fiveDayData.temp).
-                     append($("<li>").text(fiveDayData.wind).
-                     append($("<li>").text(fiveDayData.humid).
-                     append($("<li>").text(fiveDayData.uv))))));
-           }
+    };
+    
+    // function to fetch and display five day forcast
+    const fiveDayForecast = (location, latitude, longitude) => {
+        const apiFiveDay = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&exclude=current,minutely,hourly,alerts&appid=" + apiKey;
+        fetch(apiFiveDay).then((response) => {
+            response.json().then((data) => {
+                (data)
+                for(i = 1; i <= 5; i++) {
+                    fiveDayData = {
+                        name: location,
+                        date: data.daily[i].dt,
+                        temp: data.daily[i].temp.day,
+                        wind: data.daily[i].wind_speed + " MPH",
+                        humid: data.daily[i].humidity + "%",
+                        uv: data.daily[i].uvi
+                    }
+                    //  display data
+                    const forecastCard = $("#day" + [i]);
+                    forecastCard.append($("<li>").text(fiveDayData.name).
+                    append($("<li>").text(fiveDayData.temp).
+                    append($("<li>").text(fiveDayData.wind).
+                    append($("<li>").text(fiveDayData.humid).
+                    append($("<li>").text(fiveDayData.uv))))));
+                }
+                // add key to storage object and send object to save function
+                locationObj.fiveDay = fiveDayData;
+                saveLocationObj(locationObj);
+            })
         })
-    })
-};
-
-
-// function to save and load the location object that contains current and five day forecast 
-const saveLocationObj = () => {
-    const storageObj = JSON.stringify(locationObj);
-    localStorage.setItem("weather", storageObj);
-}
-const loadPage = () => {
-
-}
-
-// event listeners
-$(".search-btn").on("click", (event) => {
+    };
+    
+    // function to generate and display past query buttons
+    // function to save and load the location object that contains current and five day forecast 
+    const saveLocationObj = (obj) => {
+        pastSearchs.append($("<li>").
+        text(obj.current.name).
+        addClass("text-center p2 border-2 border-gray-100").
+        on("click", () => {
+            currentForecast();
+            
+        }));
+        const storageObj = JSON.stringify(obj);
+        localStorage.setItem("weather", storageObj);
+    }
+    const loadPage = () => {
+        
+    }
+    
+    // event listeners
+    $(".search-btn").on("click", (event) => {
     event.preventDefault();
     currentForecast(inputData.value);
-    setTimeout(saveLocationObj, 1000);
+    // setTimeout(saveLocationObj, 1000);
     inputData.value = "";
     
 });
